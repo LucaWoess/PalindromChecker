c16a263912d95fc55b653503d1a3f04e6829d6a8
LucaWoess <luwoess@tsn.at>
2020-02-27 09:55:12 +0100

    Added: Palindrom rekursiv Method

--- src/palindromMain.java
+++ src/palindromMain.java
@@ -1,9 +1,26 @@
+import java.util.Scanner;
 
-public class palindromMain {
+public class palindromMain 
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		Scanner Sc = new Scanner(System.in);
+		System.out.print("Eingabe: ");
+		String input = Sc.next();
+		if(palCheck(input)) {System.out.println("Der eingegeben String '"+input+"' ist ein Palindrom!");}
+		else {System.out.println("Der eingegeben String '"+input+"' ist kein Palindrom!");}
+	}
 
+	public static boolean palCheck(String input) 
+	{
+		String viceVersaString = "";
+		for(int i=input.length()-1;i>=0;i--)
+		{
+			viceVersaString = viceVersaString + input.charAt(i); 
+		}
+		System.out.println(viceVersaString);
+		return (viceVersaString.equalsIgnoreCase(input));
 	}
 
 }
